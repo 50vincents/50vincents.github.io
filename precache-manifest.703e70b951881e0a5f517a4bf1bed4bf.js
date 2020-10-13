09fb5530eba631496b9c0ff6b247a7248b01d0af
50vincents <vincethai035@gmail.com>
2020-10-12 17:22:16 -0700

    Updates

---
+++ precache-manifest.703e70b951881e0a5f517a4bf1bed4bf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b365bbeb5f6347773f583b96749c17",
+    "url": "/portfolioo/index.html"
+  },
+  {
+    "revision": "36375718d90ea2641d96",
+    "url": "/portfolioo/static/css/main.1c56270b.chunk.css"
+  },
+  {
+    "revision": "947677ee0b07d620f854",
+    "url": "/portfolioo/static/js/2.f604a491.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolioo/static/js/2.f604a491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36375718d90ea2641d96",
+    "url": "/portfolioo/static/js/main.f91bf4a0.chunk.js"
+  },
+  {
+    "revision": "4f62737692b606e9ac18",
+    "url": "/portfolioo/static/js/runtime-main.6d0aa155.js"
+  },
+  {
+    "revision": "eea742314c749181101c2f0332b4d661",
+    "url": "/portfolioo/static/media/AMAZON.eea74231.png"
+  },
+  {
+    "revision": "9853d5100b66f4d00a491ea4fe551161",
+    "url": "/portfolioo/static/media/IG.9853d510.png"
+  },
+  {
+    "revision": "f6c6cffccf0f402a38c8366ba35e93b3",
+    "url": "/portfolioo/static/media/MOB.f6c6cffc.png"
+  },
+  {
+    "revision": "77bbffad510f983a0de33e1dba89e05e",
+    "url": "/portfolioo/static/media/PROF.77bbffad.jpg"
+  },
+  {
+    "revision": "2c14393e7f1714cbc13cb486eb461422",
+    "url": "/portfolioo/static/media/RESUMELATEST.2c14393e.pdf"
+  },
+  {
+    "revision": "1d2b6300e55104f1560ac7d4df28c5de",
+    "url": "/portfolioo/static/media/SPOTIFY.1d2b6300.png"
+  },
+  {
+    "revision": "1949245f0f825f884d17e4df12cadbb4",
+    "url": "/portfolioo/static/media/TIKTOK.1949245f.png"
+  },
+  {
+    "revision": "ed2461c6935f741ca2a86e0e62780b46",
+    "url": "/portfolioo/static/media/discord.ed2461c6.png"
+  }
+]);
